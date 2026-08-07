ceb00c4981e95a0af7ec7ea9c9b1786ecaefdea8
HafeezU23 <hafeezullahkhan2004@gmail.com>
2026-08-07 17:33:18 +0500

    feat: initialize App component and create Home page layout with hero, category, and arrivals sections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,13 @@ import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import Category from './features/ProuductCatalog/components/Category'
 import NewArrivals from './features/ProuductCatalog/components/NewArrivals'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-between overflow-x-hidden">
       <Navbar />
-      <HeroSection />
-      <NewArrivals />
-      <Category />
+      <Home/>
       <Footer />
 
     </div>
