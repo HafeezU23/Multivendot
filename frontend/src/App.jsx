1c13cca6cf04146fd3d89160df51ee5c15e00392
AmiAmir121 <ameenaamir121@gmail.com>
2026-08-07 16:42:49 +0500

    Edited App.jsx and created Category component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import Category from './components/Category'
 import NewArrivals from './features/ProuductCatalog/components/NewArrivals'
-import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-between">
+    <div className="">
       <Navbar />
       <HeroSection />
       <NewArrivals />
+      <Category />
       <Footer />
+
     </div>
   )
 }
 
-export default App
+export default App;
