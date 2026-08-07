0f5fb63107e01cc776216665d7a0bbdad6682d95
HafeezU23 <hafeezullahkhan2004@gmail.com>
2026-08-07 17:12:21 +0500

    make UI mobile friendly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import NewArrivals from './features/ProuductCatalog/components/NewArrivals'
 
 const App = () => {
   return (
-    <div className="min-h-screenbg-gray-50 flex flex-col justify-between">
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-between overflow-x-hidden">
       <Navbar />
       <HeroSection />
       <NewArrivals />
