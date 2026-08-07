9e45da3da1cd49994b712d320e40e15314df56ec
AmiAmir121 <ameenaamir121@gmail.com>
2026-08-07 17:29:02 +0500

    Created Login Page and deleted sing up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,17 @@ import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import Category from './components/Category'
 import NewArrivals from './features/ProuductCatalog/components/NewArrivals'
+// import Login from './features/Authentication/components/Login'
 
 const App = () => {
   return (
-    <div className="min-h-screenbg-gray-50 flex flex-col justify-between">
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-between  ">
       <Navbar />
       <HeroSection />
       <NewArrivals />
       <Category />
       <Footer />
+
 
     </div>
   )
